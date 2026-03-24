83bbe9af1eacecae8d3a5b563fe7d5c1de759c9e
tuanqweqwe13-design <tuanqweqwe13@gmail.com>
2026-03-24 10:11:09 +0700

    Update sodohoocner.c

--- sodohoocner.c
+++ sodohoocner.c
@@ -6,24 +6,34 @@
 void sodohoocner(double a[], int n, double c){
     double b[MAX + 1];
     double hamKQ [MAX + 1];
-    
+
+    printf ("\n------So do Hoocner------\n");
+   
     for (int i = 0; i <=n; i++) 
     {
         b[i] = a[i];
+        printf ("%+10f ", b[i]);
     }
-
+    printf ("(phuong trinh ban dau)");
+    printf ("\n");
+    
     for (int k = n; k >= 0; k--)
     {
+        printf ("%+10f ", b[0]);
         for (int i = 1; i <= k; i++)
         {
             b[i] = b[i - 1] * c + b[i];
+            printf ("%+10f ", b[i]);
         }
+        printf (" (y^%d)", n - k); 
+        printf ("\n");
+        
         hamKQ[k] = b[k];
     }
     printf ("Ham so can tim la P(y %+lf) = ", c);
     for (int k = 0; k <= n; k++ )
         {
-            printf (" %+fx^%d ", hamKQ[k], n - k);
+            printf (" %+fy^%d ", hamKQ[k], n - k);
         }
 }
 int main() {
@@ -38,7 +48,8 @@ int main() {
         printf ("\n ==========CHUONG TRINH SO DO HOOCNER==========");
         printf ("\n[1]. Nhap ham mau");
         printf ("\n[2]. Nhap ham tu chon");
-        printf ("\n[3]. Exit\n");
+        printf ("\n[3]. Exit");
+        printf ("\n Nhap lua chon cua ban : ");
         scanf ("%d", &luachon);
 
         if (luachon > 3 || luachon <1) 
@@ -76,6 +87,7 @@ int main() {
     printf ("\nban co muon quay ve menu khong (Y de tiep tuc, cac nut khac de dung chuong trinh) : ");
     scanf (" %c", &luachon);
     }while (luachon == 'y' || luachon == 'Y');
+    printf ("Chuong trinh da dung, tam biet");
 
     return 0;
 }
